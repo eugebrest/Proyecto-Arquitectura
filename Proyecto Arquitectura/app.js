04a1f3c50c17e5fa7aeb520ca10f947abfd14e68
Aldana-Lorenzo <aldana.lorenzo@gmail.com>
2023-08-20 14:24:26 -0300

    Modificando el puerto

--- Proyecto Arquitectura/app.js
+++
@@ -1,8 +0,0 @@
-const express= require ("express");
-const app= express ();
-const path= require ('path')
-app.listen (3000,()=>{
-    console.log ("servidor corriendo")
-});
-app.get ('/', (req,res)=>{res.send ("Bienvenidos al sitio")})
-app.get ('/', (req,res)=>{res.sendFile (path.join (__dirname, '/views/index.html'))});
