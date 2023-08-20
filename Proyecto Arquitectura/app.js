27137ca7e589a76e83ac1e44c6f7b873df44061a
eugebrest <eugeniabrest@hotmail.com>
2023-08-20 02:13:48 -0300

    levantando el servidor

---
+++ Proyecto Arquitectura/app.js
@@ -0,0 +1,8 @@
+const express= require ("express");
+const app= express ();
+const path= require ('path')
+app.listen (3000,()=>{
+    console.log ("servidor corriendo")
+});
+app.get ('/', (req,res)=>{res.send ("Bienvenidos al sitio")})
+app.get ('/', (req,res)=>{res.sendFile (path.join (__dirname, '/views/index.html'))});
