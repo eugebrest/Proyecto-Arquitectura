a566e3379bb45bc417d6924ab0be995ea8f1ab29
eugebrest <eugeniabrest@hotmail.com>
2023-09-09 00:33:15 -0300

    cambios en app y main controller

--- src/routes/servicios.js
+++ src/routes/servicios.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const serviciosController = require('../controllers/serviciosController');
 
 router.get('/servicios', serviciosController.servicios); //es necesario que diga otra vez '/servicios' o podria ser solo '/' o alguna otra palabra para referenciar servicios?
-
+//string con ruta base del recurso y como es ruta empieza con barra. Cuando es detalle no agregan la ruta base
 module.exports = router;
